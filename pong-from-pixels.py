1dfacf543fb03eddde4d17683de4369a5d5739f9
Omkar Vedpathak <omkarv@amazon.com>
2018-12-30 09:52:35 -0800

    Add base python algo from Karpathy Gist with extra comments for clarity
    This is the source file:
    https://gist.github.com/karpathy/a4166c7fe253700972fcbc77e4ea32c5
    
    I also added some comments / notes

---
+++ pong-from-pixels.py
@@ -0,0 +1,159 @@
+""" Majority of this code was copied directly from Andrej Karpathy's gist:
+https://gist.github.com/karpathy/a4166c7fe253700972fcbc77e4ea32c5 """
+
+""" Ideas for things to do:
+* Have an UP, DOWN and STAY (new) state.  UP is > 0.66, 0.33 <= STAY <= 0.66, DOWN < 0.33
+* Use Tensor flow to remove the neural network code
+* Tweak with the hyperparameters
+* Output some nice charts
+* Output a sample game A/V file, with and without the STAY state, and resulting performance differences
+"""
+
+""" Trains an agent with (stochastic) Policy Gradients on Pong. Uses OpenAI Gym. """
+import numpy as np
+import cPickle as pickle
+import gym
+
+# hyperparameters to tune
+H = 200 # number of hidden layer neurons
+batch_size = 10 # every how many episodes to do a param update?
+learning_rate = 1e-3
+gamma = 0.99 # discount factor for reward
+decay_rate = 0.99 # decay factor for RMSProp leaky sum of grad^2
+resume = True # resume from previous checkpoint?
+render = False
+
+# model initialization
+D = 80 * 80 # input dimensionality: 80x80 grid
+if resume:
+  model = pickle.load(open('save.p', 'rb'))
+else:
+  model = {}
+  model['W1'] = np.random.randn(H,D) / np.sqrt(D) # "Xavier" initialization .. dimension will be H x D
+  model['W2'] = np.random.randn(H) / np.sqrt(H) # Dimension will be H
+  
+grad_buffer = { k : np.zeros_like(v) for k,v in model.iteritems() } # update buffers that add up gradients over a batch
+rmsprop_cache = { k : np.zeros_like(v) for k,v in model.iteritems() } # rmsprop memory
+
+def sigmoid(x): 
+  return 1.0 / (1.0 + np.exp(-x)) # sigmoid "squashing" function to interval [0,1]
+
+def prepro(I):
+  """ prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
+#   print I
+  I = I[35:195] # crop
+  I = I[::2,::2,0] # downsample by factor of 2 - I think this effectively ignores color then?
+  I[I == 144] = 0 # erase background (background type 1)
+  I[I == 109] = 0 # erase background (background type 2)
+  I[I != 0] = 1 # everything else (paddles, ball) just set to 1
+  return I.astype(np.float).ravel() # ravel flattens an array
+
+def discount_rewards(r):
+  """ take 1D float array of rewards and compute discounted reward """
+  """ this function discounts from the action closest to the end of the completed game backwards
+  so that the most recent action has a greater weight """
+  discounted_r = np.zeros_like(r)
+  running_add = 0
+  print reward
+  for t in reversed(xrange(0, r.size)): # xrange is no longer supported in Python 3
+    if r[t] != 0: running_add = 0 # reset the sum, since this was a game boundary (pong specific!)
+    # the line above ensures we only start accounting for the actions before the reward was received 
+    # since once a reward is received any further actions are just noise and do not determine the result of the 
+    # game (as it has already been won)
+    running_add = running_add * gamma + r[t]
+    discounted_r[t] = running_add
+  return discounted_r
+
+def policy_forward(x):
+  """This is a manual implementation of a forward prop"""
+  h = np.dot(model['W1'], x) # (H x D) . (D x 1) = (H x 1) (200 x 1)
+  h[h<0] = 0 # ReLU nonlinearity - why do this?   
+  logp = np.dot(model['W2'], h) # This outputs a probability.   (1 x H) . (H x 1) = 1 (scalar)
+  p = sigmoid(logp)  # squashes output to  between 0 & 1 range
+  return p, h # return probability of taking action 2, and hidden state
+
+def policy_backward(eph, epdlogp):
+  """ backward pass. (eph is array of intermediate hidden states) """
+  """ Manual implementation of a backward prop"""
+  """ Basically its an array of the hidden states that corresponds to all the images that were
+  fed to the NN (for the entire episode, so a bunch of games) and their corresponding logp"""
+  dW2 = np.dot(eph.T, epdlogp).ravel()
+  dh = np.outer(epdlogp, model['W2'])
+  dh[eph <= 0] = 0 # backpro prelu
+  dW1 = np.dot(dh.T, epx)
+  return {'W1':dW1, 'W2':dW2}
+
+env = gym.make("Pong-v0")
+observation = env.reset()
+prev_x = None # used in computing the difference frame
+xs,hs,dlogps,drs = [],[],[],[]
+running_reward = None
+reward_sum = 0
+episode_number = 0
+while True:
+  if render: env.render()
+
+  # preprocess the observation, set input to network to be difference image
+  cur_x = prepro(observation)
+  # we take the difference in the pixel input, since this is more likely to account for interesting information
+  # e.g. motion
+  x = cur_x - prev_x if prev_x is not None else np.zeros(D) 
+  prev_x = cur_x
+
+  # forward the policy network and sample an action from the returned probability
+  aprob, h = policy_forward(x)
+  # I think this step is randomly choosing a number which is the basis of making an action decision
+  # and if the number is less than the probability of UP output from our neural network given the image
+  # then go down
+  action = 2 if np.random.uniform() < aprob else 3 # roll the dice! 2 is UP, 3 is DOWN, 0 is stay the same
+
+  # record various intermediates (needed later for backprop). 
+  # This code would have otherwise been handled by a NN library
+  xs.append(x) # observation
+  hs.append(h) # hidden state
+  y = 1 if action == 2 else 0 # a "fake label"
+  dlogps.append(y - aprob) # grad that encourages the action that was taken to be taken (see http://cs231n.github.io/neural-networks-2/#losses if confused)
+
+  # step the environment and get new measurements
+  observation, reward, done, info = env.step(action)
+  reward_sum += reward
+  drs.append(reward) # record reward (has to be done after we call step() to get reward for previous action)
+
+  if done: # an episode finished
+    episode_number += 1
+
+    # stack together all inputs, hidden states, action gradients, and rewards for this episode
+    epx = np.vstack(xs)
+    eph = np.vstack(hs)
+    epdlogp = np.vstack(dlogps)
+    epr = np.vstack(drs)
+    xs,hs,dlogps,drs = [],[],[],[] # reset array memory
+
+    # compute the discounted reward backwards through time
+    discounted_epr = discount_rewards(epr)
+    # standardize the rewards to be unit normal (helps control the gradient estimator variance) - not sure what this means
+    discounted_epr -= np.mean(discounted_epr)
+    discounted_epr /= np.std(discounted_epr)
+
+    epdlogp *= discounted_epr # modulate the gradient with advantage (Policy Grad magic happens right here.)
+    grad = policy_backward(eph, epdlogp)
+    for k in model: grad_buffer[k] += grad[k] # accumulate grad over batch
+
+    # perform rmsprop parameter update every batch_size episodes
+    if episode_number % batch_size == 0:
+      for k,v in model.iteritems():
+        g = grad_buffer[k] # gradient
+        rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + (1 - decay_rate) * g**2
+        model[k] += learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
+        grad_buffer[k] = np.zeros_like(v) # reset batch gradient buffer
+
+    # boring book-keeping
+    running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
+    print 'resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward)
+    if episode_number % 100 == 0: pickle.dump(model, open('save.p', 'wb'))
+    reward_sum = 0
+    observation = env.reset() # reset env
+    prev_x = None
+
+  if reward != 0: # Pong has either +1 or -1 reward exactly when game ends.
+    print ('ep %d: game finished, reward: %f' % (episode_number, reward)) + ('' if reward == -1 else ' !!!!!!!!')# every how many episodes to do a param update?
